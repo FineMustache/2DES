584b5390fac112a1736c9e32c088f63ee3038948
FineMustache <guihenrique2806@gmail.com>
2022-08-26 10:50:50 -0300

    telefone array

--- pwbe/Aula03/clientes/index.js
+++ pwbe/Aula03/clientes/index.js
@@ -14,6 +14,25 @@ app.get('/clientes', (req, res) => {
     let string = "select * from vw_clientes";
     con.query(string,(err,result)=>{
         if (err==null) {
+            let id
+            let repIndex = []
+            result.forEach(c => {
+                c.telefone = [c.telefone]
+                if (result.indexOf(c) > 0) {
+                    if (id == c.id_cliente) {
+                        result[(result.indexOf(c)-1)].telefone.forEach(tele => {
+                            c.telefone.push(tele)
+                        });
+
+                        repIndex.push(result.indexOf(c)-1)
+                    }
+                }
+                tel = c.telefone
+                id = c.id_cliente
+            });
+            repIndex.forEach(ind => {
+                result.splice(ind, 1)
+            });
             res.json(result)
         } else {
             res.json(err)
