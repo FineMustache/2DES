d45003159198e036bad815d5da91743139924ec1
FineMustache <guihenrique2806@gmail.com>
2022-09-29 11:05:27 -0300

    chamados

---
+++ indmo/aula06/App.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './src/pages/Login';
+import Home from './src/pages/Home';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
